21ef13f0b3899302923dd561eb0136560978d9db
Ade Ariawan <ariawan.ade@gmail.com>
2024-09-06 06:18:37 +0700

    modify vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tugas-siswa",
+  base: "https://adeandro.github.io/tugas-siswa/",
   plugins: [react()],
 });
